b1ff135308e95c5239ea986034f09129a90bb0e2
YuraBobeshko <gorobech55@gmail.com>
2019-10-30 14:10:23 +0200

    Updates

---
+++ precache-manifest.0de4770976d28edcd0da42a3a51bbeb0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed4fbe9d6127d576cfecda32c983c299",
+    "url": "/react_counter/index.html"
+  },
+  {
+    "revision": "2fc5ff10f6697cb0ef86",
+    "url": "/react_counter/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "0bb6ab8dcd1b2c345976",
+    "url": "/react_counter/static/js/2.fe44e0e4.chunk.js"
+  },
+  {
+    "revision": "2fc5ff10f6697cb0ef86",
+    "url": "/react_counter/static/js/main.79e9d1cb.chunk.js"
+  },
+  {
+    "revision": "c1d1916f1c04cd84be27",
+    "url": "/react_counter/static/js/runtime~main.d325259d.js"
+  }
+]);
